367b46ecc74e2f809e42e1cef9c00d6961cd6bf0
sujithkalakonda-techolution <sujith.kalakonda@techolution.com>
2026-08-23 00:54:52 +0530

    handled duplicate doc names in the list

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import string
 
 documents_path = Path("documents")
 
+# This dict contains words and documents list which contains that words
+index = {}
+
 for file in documents_path.iterdir():
 
     content = file.read_text()
@@ -11,8 +14,22 @@ for file in documents_path.iterdir():
     print(file.name)
     # print(content)
 
+    # Text -> words
     words = content.split()
     for word in words:
+
+        # normalize
         word = word.lower()
         word = word.strip(string.punctuation)
-        print(word)
+        # print(word)
+
+        if word:
+            # avoid adding empty string
+            if word not in index:
+                index[word] = []
+
+            # Also make sure the same document is not appended twice.
+            if file.name not in index[word]:
+                index[word].append(file.name)
+
+print(index)
